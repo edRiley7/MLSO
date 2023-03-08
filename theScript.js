09c94fe720317fdcb5ddccf39da1dbc6a94e72ec
edRiley7 <99607967+edRiley7@users.noreply.github.com>
2023-03-08 08:50:21 -0500

    Add files via upload

--- theScript.js
+++ theScript.js
@@ -4,43 +4,20 @@ let Micro = document.getElementById('Micro');
 let Phleb = document.getElementById('Phleb');
 let Heme = document.getElementById('Heme');
 
-function hoverChangeBB(){
-    BloodBank.style.backgroundColor = 'white';
+function hoverChange(event){
+    event.target.style.backgroundColor = 'white';
 }
-function revertToNormalBB(){
-    BloodBank.style.backgroundColor = 'darkred';
+function revertToNormal(event){
+    event.target.style.backgroundColor = '';
 }
-function hoverChangeC(){
-    Chemistry.style.backgroundColor = 'white';
-}
-function revertToNormalC(){
-    Chemistry.style.backgroundColor = 'goldenrod';
-}
-function hoverChangeM(){
-    Micro.style.backgroundColor = 'white';
-}
-function revertToNormalM(){
-    Micro.style.backgroundColor = 'darkgreen';
-}
-function hoverChangeP(){
-    Phleb.style.backgroundColor = 'white';
-}
-function revertToNormalP(){
-    Phleb.style.backgroundColor = 'purple';
-}
-function hoverChangeH(){
-    Heme.style.backgroundColor = 'white';
-}
-function revertToNormalH(){
-    Heme.style.backgroundColor = 'lightcoral'
-}
-BloodBank.addEventListener('mouseover',hoverChangeBB);
-BloodBank.addEventListener('mouseout',revertToNormalBB);
-Chemistry.addEventListener('mouseover',hoverChangeC);
-Chemistry.addEventListener('mouseout',revertToNormalC);
-Micro.addEventListener('mouseover',hoverChangeM);
-Micro.addEventListener('mouseout',revertToNormalM);
-Phleb.addEventListener('mouseover',hoverChangeP);
-Phleb.addEventListener('mouseout',revertToNormalP);
-Heme.addEventListener('mouseover',hoverChangeH);
-Heme.addEventListener('mouseout',revertToNormalH);
+
+BloodBank.addEventListener('mouseover',hoverChange);
+BloodBank.addEventListener('mouseout',revertToNormal);
+Chemistry.addEventListener('mouseover',hoverChange);
+Chemistry.addEventListener('mouseout',revertToNormal);
+Micro.addEventListener('mouseover',hoverChange);
+Micro.addEventListener('mouseout',revertToNormal);
+Phleb.addEventListener('mouseover',hoverChange);
+Phleb.addEventListener('mouseout',revertToNormal);
+Heme.addEventListener('mouseover',hoverChange);
+Heme.addEventListener('mouseout',revertToNormal);
